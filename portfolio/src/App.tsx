8b22d1006c63cea9f832c7f7d9703b6480d7ce47
Guillermo Muñoz <guillemunozi2003@gmail.com>
2026-09-08 12:39:16 -0500

    Update App.tsx

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.scss'
+import { Footer } from './shared/layout/Footer'
+import { Header } from './shared/layout/Header'
 
 function App() {
 
   return (
     <>
-      
+      <Header />
+      <Footer />
     </>
   )
 }
